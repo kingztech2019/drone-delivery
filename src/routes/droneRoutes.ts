cad32a9691d0be16b3ec82b7860a67d79004156b
Oluwajuwon Falore <oluwajuwon.falore@fidelitybank.ng>
2025-02-18 05:13:12 +0100

    added  routes

---
+++ src/routes/droneRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { DroneController } from '../controllers/droneController';
+
+const router = Router();
+const droneController = new DroneController();
+
+router.post('/drones', droneController.registerDrone);
+router.post('/drones/:id/load', droneController.loadDrone);
+router.get('/drones/:id/battery', droneController.getDroneBattery);
+router.get('/drones/available', droneController.getAvailableDrones);
+
+export default router;
